f78231159826133dd6c9b20e9f42e8903e3a0a8b
unknown <wang1993x@gmail.com>
2020-03-22 18:07:52 -0700

    gg

--- matlab/relu_backward.m
+++ matlab/relu_backward.m
@@ -4,6 +4,9 @@ batch_size = input.batch_size;
 % Replace the following line with your implementation.
 input_od = zeros(size(input.data));
 for b = 1:batch_size
-    
+    h_1 = input.data(:,b);
+    h = output.data(:,b);
+    grad = 0000000000000000000;
+    input_od(:,b) = output.diff(:,b)'*grad;
 end
 end
