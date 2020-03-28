a34e8a4405a021085dbbbc355a4c66a3c8215ab9
chenhao wang <wang1993x@gmail.com>
2020-03-28 13:21:36 -0700

    finished

--- matlab/test_network.m
+++ matlab/test_network.m
@@ -11,13 +11,21 @@ load lenet.mat
 %% Testing the network
 % Modify the code to get the confusion matrix
 confusion_m = zeros(10);
+correct = 0;
+
 for i=1:100:size(xtest, 2)
+    pred_labels = [];
     [output, P] = convnet_forward(params, layers, xtest(:, i:i+99));
     test_label = ytest(:, i:i+99);
     for j = 1:size(P,2)
         [prob, pred_label] = max(P(:,j));
         confusion_m(test_label(j), pred_label) = confusion_m(test_label(j), pred_label) + 1;
+        pred_labels = [pred_labels pred_label];
     end
+
+    % calculate accuracy
+    correct = correct + sum(pred_labels == test_label);
 end
+accuracy = correct/size(xtest, 2);
 %confusion_m = confusion_m/size(xtest, 2);
 
